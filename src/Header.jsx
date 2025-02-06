d7c6c82a73fdd7ed21e98201e6f7727841f9de91
Alan <alanitoyah96@gmail.com>
2025-02-06 16:07:12 -0600

    began implementation of header component

---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+// import headerLogo from '/src/assets/diamondLogo.png'
+// import React from 'react'
+
+
+// <div>
+// <header className='header'>
+//             <a href='#default' class="logo">
+//               <img src={headerLogo} className='logo'/>
+//               </a>
+//             <div className='headerRight'>
+//               <a className='' href ="home">Intro</a>
+//               <a className='' href ="home">Work</a>
+//               <a className='' href ="home">Projects</a>
+//               <a className='' href ="home">Contact</a>
+//             </div>
+//           </header>
+//           </div>
+
+//thinking of transfering header to a component
